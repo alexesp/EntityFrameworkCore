785e1a6194091b149d19920ee6b0f632b4865646
alexesp <aleizaes@hotmail.com>
2025-03-06 15:47:09 +0100

    Linq basico

--- EntityFrameworkCore.Console/Program.cs
+++ EntityFrameworkCore.Console/Program.cs
@@ -1,12 +1,31 @@
 ﻿using EntityFrameworkCore.Data;
+using Microsoft.EntityFrameworkCore;
 
-var context = new FootballLeageDbContext();
+using var context = new FootballLeageDbContext();
 
 
-//LINQ
-var teams = context.Teams.ToList();
 
-foreach(var item in teams)
-{
-    Console.WriteLine(item.Name);
-}
+//GetAllTeams();
+
+
+//var teamOne = await context.Teams.FirstAsync();
+//var teamOne = await context.Teams.FirstOrDefaultAsync();
+
+//var teamTwo = await context.Teams.FirstAsync(team => team.TeamId == 1);
+//var teamTwo = await context.Teams.FirstOrDefaultAsync(team => team.TeamId == 1);
+
+//var teamThree = await context.Teams.SingleAsync();
+//var teamThree = await context.Teams.SingleAsync(team => team.TeamId == 2);
+var teamThree = await context.Teams.SingleOrDefaultAsync(team => team.TeamId == 2);
+
+//void GetAllTeams()
+//{
+//    //LINQ
+//    var teams = context.Teams.ToList();
+
+//    foreach (var item in teams)
+//    {
+//        Console.WriteLine(item.Name);
+//    }
+
+//}
